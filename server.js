011dae4c80d5aed497fcf01e31208e273a97f2c7
Utkarsh Raj <clearnote01@gmail.com>
2016-09-13 15:28:53 +0530

    added dbase route

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/dbase', function(req, res) {
+  res.send(db);
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
