5567b1b266dffe849d8e0bb25565178f7cadc848
Utkarsh Raj <clearnote01@gmail.com>
2016-09-18 14:08:28 +0530

    some changes

--- public/js/firewizard.js
+++ public/js/firewizard.js
@@ -32,8 +32,25 @@ $(document).ready(function() {
 
   $('#loginlink').click(enableSignupform);
   $('#loginlink2').click(enableLoginform);
-
+  $('#return-to-top').fadeIn(800);  
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        
+        $('#return-to-top').fadeIn(600);    
+    }
+    else {
+        $('#return-to-top').fadeOut(600);   
+        // Else fade out the arrow
+    }
+  });
+  $('#return-to-top').click(function() {      
+    // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       
+        // Scroll to top of body
+    }, 500);
+  });
   console.log($('loginbutton'));
+  
 });
 
 document.body.onload = startSite();
