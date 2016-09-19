b21b07ed5a076f1397ee715c416fe5f4f49c5ed5
Utkarsh Raj <clearnote01@gmail.com>
2016-09-19 20:49:00 +0530

    ajax signup, login; random quote

--- public/js/firewizard.js
+++ public/js/firewizard.js
@@ -4,6 +4,21 @@ function startSite() {
 }
 
 $(document).ready(function() {
+  // jquery extend function
+$.extend(
+{
+    redirectPost: function(location, args)
+    {
+        var form = '';
+        $.each( args, function( key, value ) {
+            value = value.split('"').join('\"')
+            form += '<input type="hidden" name="'+key+'" value="'+value+'">';
+        });
+        $('<form action="' + location + '" method="POST">' + form + '</form>').appendTo($(document.body)).submit();
+    }
+});
+
+
   $('#signupbutton').click(enableSignupform);
   $('#signup').hide();
   $('#loginbutton').click(enableLoginform);
@@ -30,6 +45,60 @@ $(document).ready(function() {
     }, 1000);
   });
 
+  $('#justtest').submit(function() {
+    //console.log(this.val());
+    $.post('/justtest',{"nui":$('#fuckyou').val(), "success":0}, function(msg) {
+      console.log(msg);
+      console.log(msg.success);
+    });
+    return false;
+  });
+
+  function logmsg(msg) {
+    $('#logsignmsg').text(msg);
+  }
+
+  $('#signup').submit(function() {
+    var username = $('input[name="signname"]');
+    var password = $('input[name="signpass"]');
+    console.log(password);
+    $.post('/signup', {
+      //'signname':$("input[name='signname']"),
+      //'signpass':$("input[name='signpass']")
+      'signname':username.val(),
+      'signpass':password.val()
+    },
+    function(msg) {
+      console.log(msg);
+      enableLoginform();
+      logmsg(msg);
+    });
+    return false;
+  });
+
+  $('#login').submit(function() {
+    var dd = null;
+    var username = $('input[name="logname"]');
+    var password = $('input[name="logpass"]');
+    //console.log(password);
+    $.post('/login', {
+      //'signname':$("input[name='signname']"),
+      //'signpass':$("input[name='signpass']")
+      'logname':username.val(),
+      'logpass':password.val()
+    },
+    function(msg) {
+      //console.log(msg);
+			if (msg == 'fail') {
+				logmsg('Wrong username or password.');
+			}
+			else {
+				$.redirectPost('/login',{ logname: username.val(), logpass: password.val() });
+			}
+    });
+		return false;
+  });
+    
   $('#loginlink').click(enableSignupform);
   $('#loginlink2').click(enableLoginform);
   $('#return-to-top').fadeIn(800);  
